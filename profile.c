740517666d35ef7f2b5d4e141bd6dade650d04fb
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-09 16:46:12 +0530

    Update and rename p.c to profile.c

---
+++ profile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("\n enter your name:");
+printf("\n what is your qualification");
+printf("\n date of birth);
+printf("\n age");
+printf("\n communicational adress");
+printf("\n permenent adress");
+printf("\n how experienced you are");
+getch();
+}
+
+
