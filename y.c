2618959456a595ee2f3f0678719e9f173983f74c
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-09 16:04:55 +0530

    Create y.c

---
+++ y.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+void rotateByOne(int a[], int n)
+{
+  int i, temp;
+  temp = a[0];
+  for (i = 0; i < n-1; i++)
+     a[i] = a[i+1];
+  arr[i] = temp;
+}
+ 
+// repeat above function d times to rotate array
+void rotateArray(int a[], int n, int d)
+{
+  int i;
+  for (i = 0; i < d; i++)
+    rotateByOne(a, n);
+}
