623e9d6a1d17bdf63561cdfa5ae14ad92def241d
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-09 16:13:19 +0530

    Update and rename v.c to va.c

---
+++ va.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+void main()
+{
+    int arr[MAX_SIZE];
+    int i, N;
+
+    
+    printf("Enter size of array: ");
+    scanf("%d", &N);
+
+   
+    printf("Enter %d elements in the array : ", N);
+    for(i=0; i<N; i++)
+    
+ }
+    printf("\nElements in array are: ");
+    for(i=0; i<N; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+
+    getch();
+  return 0;  
+}
