86204de29d1b7677840a32671ee11332e160ee21
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-09 16:32:41 +0530

    Update and rename q.c to p.c

---
+++ p.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ #include<conio.h>
+void main()
+{
+  int array[100], maximum, size, c, location = 1;
+ 
+  printf("Enter the number of elements in array\n");
+  
+ 
+  printf("Enter %d integers\n", size);
+ 
+  for (c = 0; c < size; c++)
+    scanf("%d", &array[c]);
+ 
+  maximum = array[0];
+ 
+  for (c = 1; c < size; c++)
+  {
+    if (array[c] > maximum)
+      }
+ 
+  printf("Maximum element is present at location %d and it's value is %d.\n", location, maximum);
+  getch();
+}
