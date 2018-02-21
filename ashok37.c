e16d35c3a244b126b7e42cc6a97fd98252b06aab
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-21 14:29:03 +0530

    ashok37.c

--- ashok37.c
+++ ashok37.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include<conio.h>
-void main()
+int main()
 {
 int x, y, temp;
 printf("Enter the value of x and y\n");
 scanf("%d%d", &x, &y);
 printf("Before Swapping\nx = %d\ny = %d\n",x,y);
 temp = x;
-x    = y;
-y    = temp;
+x=y;
+y=temp;
 printf("After Swapping\nx = %d\ny = %d\n",x,y);
-getch();
+return 0;
 }
