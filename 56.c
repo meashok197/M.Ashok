dbdf44d96cadd5c8542b7ce02876c218010393ef
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-23 15:57:39 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+char str[100];
+
+scanf("%s",str);
+
+int alpha=0,digit=0;
+
+for(int i=0;str[i]!=0;i++)
+{
+
+if((str[i]>='a'&& str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+
+{
+	alpha++;
+
+}
+
+if(str[i]>='0' && str[i]<='9')
+
+
+{
+
+digit++;
+
+}
+
+}
+if(alpha>=1 && digit>=1)
+
+{
+
+printf("yes");
+
+}
+else
+{
+	printf("no");
+}
+return 0;
+}
