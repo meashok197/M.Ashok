193183acf476603de9c2902caad554cfeba5077b
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-26 13:16:37 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main(void)
+
+{
+
+int a,b,c;
+
+scanf("%d%d",&a,&b);
+
+c=a+b;
+
+printf("%d\n",c);
+
+if(c%2==0)
+
+{
+
+printf("no is even");
+
+}
+
+else
+
+{
+
+printf("no is odd");
+
+}
+
+return 0;
+
+}
