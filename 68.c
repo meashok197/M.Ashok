d3566c75d9bb79a73632a5cbb096b46c61fe6d80
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-26 13:48:51 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter a number");
+scanf("%d",&a);
+if((a%7)==0)
+printf("tne number is multiple of 7");
+else
+printf("tne number is not multiple of 7");
+getch();
+}
