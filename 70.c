39cccf52abcdaf802e8d512da1a370cde6a2bc04
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-26 14:11:51 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter a number ");
+scanf("%d",&a);
+a=a*2;
+printf("the nearest greater power of 2 for given number is %d",a);
+getch();
+}
