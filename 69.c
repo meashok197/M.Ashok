473ac744d60b7436c9a3e1d0b6d5264ace2b8e1c
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-26 14:01:04 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter two numbers");
+scanf("%d%d",&a,&b);
+c=a-b
+if((c%2)==0)
+printf("\n the difference of the number is even number");
+else
+printf("\n the difference of the number is odd number");
+getch();
+}
