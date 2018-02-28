2a4c18feacace2cb7819f461b6f8feef34f4699c
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-28 14:03:07 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+clrscr();
+printf("\n enter the testing numbers");
+scanf("%d,&b,&c");
+printf("\n enter a number");
+scanf("%d",a);
+if(b<a<c)
+printf("\n yes");
+else
+printf("\n no");
+getch();
+}
