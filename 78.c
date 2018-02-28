21c4fdfa4127846b5ecf70126463d223f33bb280
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-28 14:21:45 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+void main()
+int a;
+clrscr();
+printf("\n enter a number");
+scanf("%d",&a);
+if(a/13==0)
+printf("\n yes");
+else
+printf("\n no");
+getch();
+}
