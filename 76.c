24055136736f3f7782d25b61217b675712986877
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-02-28 14:58:37 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d=0;
+printf("\n enter a number");
+scanf("%d",&a);
+for(i=2;i<=a;i++)
+{
+if(a%i==0)
+{
+d=1;
+break;
+}}
+if(d==1)
+printf("\n it is a prime number");
+else
+printf("\n it is not prime number");
+getch();
+}
