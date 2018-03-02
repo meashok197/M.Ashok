aa9cd313f92b518f2bbd11a97f32eef3fd03d622
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-02 15:43:25 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+int n, reversedInteger = 0, remainder, originalInteger;
+
+
+
+printf("Enter an integer: ");
+
+scanf("%d", &n);
+
+
+
+originalInteger = n;
+
+
+
+
+
+while( n<=1000 )
+
+{
+
+remainder = n%10;
+
+reversedInteger = reversedInteger*10 + remainder;
+
+
+n /= 10;
+
+}
+
+
+
+
+
+if (originalInteger == reversedInteger)
+
+printf("yes");
+
+else
+
+printf("no");
+
+
+
+return 0;
+
+}
+
+
+
+
