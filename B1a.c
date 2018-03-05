a6e9415544cb8f4cf8d2a539a4adab58b299b824
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-05 14:11:54 +0530

    Rename B1.c to B1a.c

---
+++ B1a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+str[50];
+printf("\n enter a string");
+scanf("%s",&str),
+str=strrev();
+printf("\n ypur reversed string is %s",str);
+getch();
+}
