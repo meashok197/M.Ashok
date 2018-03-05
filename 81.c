f2b6f887e0d4430821b5c3d457926f35712b71dc
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-05 13:17:46 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float l,b,area;
+clrscr();
+Printf("\n enter length of your field in mm");
+scanf("%d",&l);
+Printf("\n enter breadth of your field in mm");
+scanf("%d",&b);
+area=(l*b);
+Printf("\n your area of the field %d");
+getch();
+}
