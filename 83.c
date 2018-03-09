d2ad3675f8d0b96fa0301da96474ec415f14fe2c
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-09 15:58:00 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+	
+  int a,b,c;
+	
+  int i;
+
+
+
+scanf("%d %d",&a,&b);
+
+if(i%2==0)
+
+c=a/b;
+
+else
+
+c=a%b;
+
+
+
+
+
+printf("\n%d",c);
+
+
+
+return 0;
+
+
+ }
+©
+2018 GitHub, Inc.
+
+
+
