dde890f32ec1b4b4fee42976097fd3e191aacd38
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-14 16:11:20 +0530

    Rename 90.c to 90a.c

---
+++ 90a.c
@@ -0,0 +1,48 @@
+#include<conio.h>
+
+#include<stdio.h>
+
+void main()
+
+{
+
+int e;
+
+public class Equals
+{
+
+
+
+public static void main(String[] args) {
+
+
+
+Scanner sc= new Scanner(System.in);
+		
+
+System.out.println("enter 1 word is ");
+
+String s,s1,s2;
+
+s=sc.nextLine();
+
+System.out.println("enter 2 word is ");
+
+s1=sc.nextLine();
+
+boolean a =s.equalsIgnoreCase(s1);
+
+System.out.println("compare without consider case "+a);
+
+
+
+
+
+}
+
+
+
+}
+
+
+
