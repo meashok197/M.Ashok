3d14b327c4a8d6efef40ea358c1ef1d24b0d2d61
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-28 16:13:16 +0530

    Create 100as.c

---
+++ 100as.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+
+int main() 
+
+{
+
+int n,remainder,prod=1;
+
+
+scanf("%d",&n);
+
+while(n!=0)
+
+{
+
+
+remainder=n%10;
+
+prod=prod*remainder;
+
+n=n/10;
+
+}
+
+printf("\n%d",prod);
+
+return 0;
+
+}
+
