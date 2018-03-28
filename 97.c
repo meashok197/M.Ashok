5baf6861f577b17f425e37d1c28de3b703008955
meashok197 <35687805+meashok197@users.noreply.github.com>
2018-03-28 16:09:18 +0530

    Create 97.c

---
+++ 97.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+
+int n,temp,rev=0;
+
+scanf("%d",&n);
+
+while(n!=0)
+
+{
+
+temp=n%10;
+
+rev=rev*10+temp;
+
+n=n/10;
+
+}
+
+printf("%d",rev);
+
+return 0;
+
+}
